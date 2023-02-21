bcd60676873e25ccb91a294e911724c6445b451b
Anastasios Vournazidis <tasosvournazidis@gmail.com>
2023-02-21 19:32:46 +0200

    Add documentation to files and README.md

--- domain/src/main/java/com/example/domain/repository/UpcomingSportsEventsRepository.kt
+++ domain/src/main/java/com/example/domain/repository/UpcomingSportsEventsRepository.kt
@@ -4,6 +4,7 @@ import com.example.domain.model.DomainResult
 import com.example.domain.model.upcomingsportsevents.SportsDomainModel
 import kotlinx.coroutines.flow.Flow
 
+/** Interface which communicates with the data layer to implement the calls */
 interface UpcomingSportsEventsRepository {
     fun getUpcomingSportsEvents(): Flow<DomainResult<List<SportsDomainModel>>>
 }
